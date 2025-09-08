615cf83b7973fb65456d7a4b8dd1d569e40c7113
Florian Maurer <maurer@fh-aachen.de>
2025-09-08 10:17:16 +0200

    add test case to add solar

--- tests/test_psa.py
+++ tests/test_psa.py
@@ -55,3 +55,51 @@ def test_various_steps(n_timesteps):
         results.total_yearly_costs_eur
         == results.energy_costs_eur + results.grid_energy_costs_eur + results.grid_capacity_costs_eur
     )
+
+
+SOLAR_PROFILE_HOURLY = [0] * 6 + [0.2, 0.4, 0.6, 0.8, 1, 1, 1, 1, 0.8, 0.6, 0.4, 0.2] + [0] * 6
+
+
+def test_add_solar():
+    n_timesteps = 48
+    solar_profile = SOLAR_PROFILE_HOURLY * (1 + 50 // len(SOLAR_PROFILE_HOURLY))
+    solar_profile = solar_profile[0:n_timesteps]
+    config = Config(
+        "test_config",
+        consumption_timeseries=[1] * n_timesteps,
+        hours_per_timestep=1,
+        n_timesteps=n_timesteps,
+        price_timeseries=pd.DataFrame({"grid": [0.3] * n_timesteps, "consumption_site": [0] * n_timesteps}),
+        add_solar=True,
+        solar_generation_timeseries=pd.DataFrame({"grid": 0, "consumption_site": solar_profile}),
+        interest_rate=0,
+    )
+    psa = PeakShavingAnalyzer(config=config)
+    results = psa.optimize()
+
+    # check that an inverter and storage is available
+    assert results.inverter_capacity_kw >= 5
+    assert results.storage_capacity_kwh >= 10
+    assert results.grid_capacity_kw == 1
+    assert results.solar_capacity_kwp >= 3
+
+    # energy costs are now much lower
+    assert results.energy_costs_eur < 400
+    assert isclose(results.grid_capacity_costs_eur, config.grid_capacity_price * 1)
+    assert isclose(results.inverter_invest_eur, results.inverter_capacity_kw * config.inverter_cost_per_kw)
+
+    # sum of investment should match
+    assert (
+        results.total_invest_eur == results.solar_invest_eur + results.inverter_invest_eur + results.storage_invest_eur
+    )
+
+    # annuities should match
+    assert (
+        results.total_yearly_costs_eur
+        == results.energy_costs_eur
+        + results.grid_energy_costs_eur
+        + results.grid_capacity_costs_eur
+        + results.solar_annuity_eur
+        + results.inverter_annuity_eur
+        + results.storage_annuity_eur
+    )
